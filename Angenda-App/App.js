fab89872dbeb925eab08e05727e2cb417e98e1df
ndembi <ndembi_x@etna-alternance.net>
2018-10-23 11:54:41 +0200

    :sparkles: update

--- Angenda-App/App.js
+++ Angenda-App/App.js
@@ -1,36 +1,25 @@
 import React from 'react';
 import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 import * as firebase from 'firebase';
-import './app';
+import SignIn from './app/components/sign-in/sign-in';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDJjUeEkm7-dApWv3nKATxoPcAtROhruFg",
-  authDomain: "mobagenda-1d650.firebaseapp.com",
-  databaseURL: "https://mobagenda-1d650.firebaseio.com",
-  projectId: "mobagenda-1d650",
-  storageBucket: "mobagenda-1d650.appspot.com"
-}
-const firebaseObject = firebase.initializeApp(firebaseConfig);
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDJjUeEkm7-dApWv3nKATxoPcAtROhruFg",
+//   authDomain: "mobagenda-1d650.firebaseapp.com",
+//   databaseURL: "https://mobagenda-1d650.firebaseio.com",
+//   projectId: "mobagenda-1d650",
+//   storageBucket: "mobagenda-1d650.appspot.com"
+// }
+// const firebaseObject = firebase.initializeApp(firebaseConfig);
 export default class App extends React.Component { 
   componentDidMount() {
 
-    console.log(firebaseObject.auth);
+    // console.log(firebaseObject.auth);
     
   }
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+    <SignIn/>
     );
   }
 }
